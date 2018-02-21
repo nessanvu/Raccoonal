84df9428313b013d6eb62a8e0c4fefb43e574af1
Zehua Chen <zec023@ucsd.edu>
2018-02-21 01:46:53 -0800

    records add to calendar

--- routes/home.js
+++ routes/home.js
@@ -8,5 +8,15 @@ exports.view = function (req, res) {
 };
 
 exports.parseJson = function (req, res) {
-  console.log("ayyy");
+  console.log(req.body);
+  res.status(200).end();
+  var t = new Date();
+  var m = t.getMonth();
+  var d = t.getDate();
+  for (var i = 0; i < data.dates.length; i++) {
+    if (data.dates[i].month == m && data.dates[i].dateNumber == d) {
+      data.dates[i].records.push(req.body);
+      console.log(data.dates[i].records);
+    }
+  }
 };
